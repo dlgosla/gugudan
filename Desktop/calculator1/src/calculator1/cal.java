8f1518fa297052ae7396ca03033fc724fba9e058
dlgos <dlgoslaek1@gmail.com>
2020-03-07 07:46:19 +0900

    2단 3단

--- Desktop/calculator1/src/calculator1/cal.java
+++ Desktop/calculator1/src/calculator1/cal.java
@@ -5,7 +5,6 @@ public class cal {
 		
 	for(int i=2;i<4;i++) {
 		for(int j=1;j<10;j++) {
-		System.out.println(i+"*"+j+"="+i*j+"\n");
-	}}
-	}
+		System.out.println(i+"*"+j+"="+i*j+"\n"); //ctrl+alt+아래/위 화살표
+	}}	}
 }
